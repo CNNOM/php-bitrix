615ad9292ac26f4ac7123f0633d84ee60b6e7cec
Zenkov <zenkov.m700@mail.ru>
2025-04-09 14:16:41 +0300

    изменён вариант подключения класса

--- modules/testmodule2.custom/include.php
+++ modules/testmodule2.custom/include.php
@@ -3,7 +3,4 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 require_once __DIR__ . "/functions.php";
 require_once __DIR__ . "/constants.php";
-
-\Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-    '\Local\TestModule\HelloManager' => '/local/modules/testmodule.custom/lib/TestModule/HelloManager.php',
-]);
+require_once __DIR__ . "/lib/TestModule/HelloManager.php";
